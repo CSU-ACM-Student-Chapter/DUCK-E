48dddc7350b40fbf340b37d8206ed3d423ec8190
Jaden Jefferson <jadenjefferson18@gmail.com>
2025-12-14 08:13:16 -0500

    Update constants.py

--- ducke/constants/constants.py
+++ ducke/constants/constants.py
@@ -1,4 +1,4 @@
-import os
+import os, mysql.connector#, sqlite3
 
 '''
 PURPOSE
@@ -17,25 +17,50 @@ NOTE
 2. It doesn't have the needed modules for variable creation
 '''
 
-# Access to Resources Folder
+# Access to Resource Directories
 RESOURCES_FOLDER= os.path.join(os.getcwd(), f'ducke\\resources\\')
 RESOURCES_QUESTIONS_FOLDER= os.path.join(os.getcwd(), f'ducke\\resources\\questions\\')
 RESOURCES_RESPONSES_FOLDER= os.path.join(os.getcwd(), f'ducke\\resources\\responses\\')
 
-# Access to all Files within Resources Folder
+# Access to all Files within Resources Questions Directory
 RESOURCES_QUESTIONS_AZ_900_FILE= os.path.join(os.getcwd(), f'ducke\\resources\\questions\\az 900.csv')
 RESOURCES_QUESTIONS_DATA_STRUCTURES_FILE=os.path.join(os.getcwd(), f'ducke\\resources\\questions\\data structure basics.csv')
 RESOURCES_QUESTIONS_NETWORKING_BASICS_FILE=os.path.join(os.getcwd(), f'ducke\\resources\\questions\\networking basics.csv')
 
+# Access to all Response Text Files
 RESOURCES_RESPONSES_QUESTION_CHEATER= os.path.join(os.getcwd(), f'ducke\\resources\\responses\\question_cheater.txt')
 RESOURCES_RESPONSES_QUESTION_CORRECT= os.path.join(os.getcwd(), f'ducke\\resources\\responses\\question_correct.txt')
 RESOURCES_RESPONSES_QUESTION_INCORRECT= os.path.join(os.getcwd(), f'ducke\\resources\\responses\\question_incorrect.txt')
 
+# Access to denied request messages
 ACCESS_REQUEST_MESSAGE="❌ I don't have permissions to complete this command in this channel. Please grant access in the Discord Developer Portal."
 
-POINTS_DB_PATH=os.path.join(os.getcwd(), f'ducke\\resources\\database\\points.db') #For local testing with sqlite
-FLASH_EVENTS_DB_PATH=os.path.join(os.getcwd(), f'ducke\\resources\\database\\flash_events.db')  #For local testing with sqlite
+# MySQL Connection
+MYSQL_CONNECTION = mysql.connector.connect(
+    host=os.getenv('DATABASE_HOSTNAME'),
+    user=os.getenv('DATABASE_USERNAME'),
+    password=os.getenv('DATABASE_PASSWORD'),
+    database=os.getenv('DATABASE_NAME')
+)
 
+# Time Constants
 MINUTE_IN_SECONDS=60
 HOUR_IN_SECONDS=3600
 DAY_IN_SECONDS=86400
+
+'''
+For local testing with sqlite3
+
+- Uncomment below code blocks
+- Uncomment import of sqlite3 above
+- Comment out mysql.connector code blocks above
+- Ensure points.db exists in the resources/database/ directory
+- Adjust code in points.py accordingly
+  - Replacing MYSQL_CONNECTION with SQLITE_CONNECTION
+  - Adjusting SQL syntax where necessary such as %s to ?
+'''
+# Access to SQLite Database Files
+# POINTS_DB_PATH=os.path.join(os.getcwd(), f'ducke\\resources\\database\\points.db')
+
+# SQLite Connection
+# SQLITE_CONNECTION = sqlite3.connect(POINTS_DB_PATH)
